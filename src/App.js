050d8643caa3e7a19ac851c1a41d43e0d4c7c8c6
Shokupanman <Harrisonkaidehancock@gmail.com>
2019-11-14 18:09:07 -0700

    owata

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import CurrencyHOC from './Components/CurrencyConverter'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <h2>Higher Order Component</h2>
+        <CurrencyHOC />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
